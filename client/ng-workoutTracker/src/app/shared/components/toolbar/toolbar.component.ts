e06e2eaa01eb6471c6a6e25867a16657a4afbfa7
Søren <soren.skieller@gmail.com>
2019-11-05 21:03:50 +0100

    toolbar just fucking work :D

--- client/ng-workoutTracker/src/app/shared/components/toolbar/toolbar.component.ts
+++ client/ng-workoutTracker/src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,7 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../services';
 import { Router } from '@angular/router';
-
+ // https://angular.io/tutorial/toh-pt5#add-a-hero-detail-route
+ // https://www.techiediaries.com/angular-router-routerlink-navigate-navigatebyurl/
+ // https://angular.io/api/common/NgIf
+ // https://angular.io/guide/router#route-parameters
+ // https://material.angular.io/components/button/overview
+ // http://localhost:3000/api-docs/#/
 @Component({
   selector: 'wt-toolbar',
   templateUrl: './toolbar.component.html',
